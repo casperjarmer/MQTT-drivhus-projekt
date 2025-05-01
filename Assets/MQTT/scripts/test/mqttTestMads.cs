411ba60e5a1287109089f68513ae27554f8c68bc
Mads Jensen <madsj20@student.sdu.dk>
2025-05-01 11:59:32 +0200

    Added fan

--- Assets/MQTT/scripts/test/mqttTestMads.cs
+++ Assets/MQTT/scripts/test/mqttTestMads.cs
@@ -10,15 +10,21 @@ public class MQTTClientMads : MonoBehaviour
 {
     private MqttClient client;
     // private string broker = "test.mosquitto.org"; // Another free online broker
-    //private string broker = "mqtt.eclipseprojects.io";
-    private string broker = "10.126.128.222"; // Used to connect to a lokal private broker
+    private string broker = "mqtt.eclipseprojects.io";
+    //private string broker = "10.126.128.222"; // Used to connect to a lokal private broker
 
     private string climateRoofMotorTopic = "climate_roof_motor"; // Topic for motor
     public string RoofMotorValue = ""; // Stores received motor value
+    private string climateFanMotorTopic = "climate_fan_motor"; // Topic for fan
+    public string fanMotorValue = ""; // Stores received fan value
 
     // The gameobjects in the scene
     public GameObject MotorRod;
     public GameObject Roof;
+    public GameObject Fan;
+
+    private int fanRotationSpeed = 1000; // Rotation speed in degrees per second
+    private bool rotateFan = false; // Flag to control fan rotation
 
     // Animation
     public Animator RoofAnimator;
@@ -26,10 +32,10 @@ public class MQTTClientMads : MonoBehaviour
 
     void Start()
     {
-        RoofAnimator.SetBool("open", true);
-        MotorRodAnimator.SetBool("open", true);
+        //RoofAnimator.SetBool("open", true);
+        //MotorRodAnimator.SetBool("open", true);
 
-        StartCoroutine(Test());
+        //StartCoroutine(Test());
 
         Application.runInBackground = true;  // Ensures the game runs even when out of focus, so you can click on the webpage and see the outpot in Unity 
 
@@ -46,6 +52,8 @@ public class MQTTClientMads : MonoBehaviour
             Debug.Log("Connected to MQTT Broker!");
             // How to subscribe to the topics 
             client.Subscribe(new string[] { climateRoofMotorTopic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+            client.Subscribe(new string[] { climateFanMotorTopic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+
         }
         else
         {
@@ -63,6 +71,10 @@ public class MQTTClientMads : MonoBehaviour
             RoofMotorValue = message;
 
         }
+        if (e.Topic == climateFanMotorTopic)
+        {
+            fanMotorValue = message;
+        }
     }
 
     void Update()
@@ -76,6 +88,16 @@ public class MQTTClientMads : MonoBehaviour
             StartCoroutine(Retract());  // Start the coroutine to blink material
         }
 
+        if (fanMotorValue == "f")  // If fanValue is "f"
+        {
+            Fan.transform.Rotate(new Vector3(0, fanRotationSpeed, 0) * Time.deltaTime);
+        }
+
+        if (fanMotorValue == "s")  // If fanValue is "s"
+        {
+            Fan.transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime);
+        }
+
     }
 
     private IEnumerator Extend()
@@ -87,14 +109,6 @@ public class MQTTClientMads : MonoBehaviour
 
     private IEnumerator Retract()
     {
-        RoofAnimator.SetBool("open", false);
-        MotorRodAnimator.SetBool("open", false);
-        yield return new WaitForSeconds(0.5f);
-    }
-
-    private IEnumerator Test()
-    {
-        yield return new WaitForSeconds(3f);
         RoofAnimator.SetBool("open", false);
         MotorRodAnimator.SetBool("open", false);
         yield return new WaitForSeconds(0.5f);
